699104e3af27a345a3af5c9c6b8b6ed60de1964a
AntaresMugisho <antaresmugisho@gmail.com>
2022-07-24 20:04:56 +0200

    Refactorisation du code

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-import http.server
-import socketserver
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
 import os
 
 from netifaces import interfaces, ifaddresses, AF_INET
 
+HOST = "0.0.0.0"
+PORT = 13200
 
 def get_ip():
-    addresses = [i['addr'] for i in ifaddresses("wlp3s0").setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+    addresses = [i["addr"] for i in ifaddresses("wlp3s0").setdefault(AF_INET, [{"addr":"No IP addr"}])]
     return " ".join(addresses)
 
-HOST = get_ip()
-PORT = 13201
+
+def launch_server():
+    handler = SimpleHTTPRequestHandler
+    server = TCPServer((HOST, PORT), handler)
+
+    print(f"Serving on : {get_ip()}:{PORT}")
+    server.serve_forever()
+
+    server.server_close()
+    print("Server stopped")
 
 
-class MyServer(http.server.SimpleHTTPRequestHandler):
-    pass
-
-handler = MyServer
-myserver = socketserver.TCPServer((HOST, PORT), handler)
-
-print(f"Serving on : {HOST}:{PORT}")
-myserver.serve_forever()
+if __name__ == "__main__":
+    launch_server()
